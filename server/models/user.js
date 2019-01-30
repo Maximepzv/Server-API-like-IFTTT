8c14202f6d69b590edb073518b58b224d2d725a7
Maximepzv <maxime.pouezevara@epitech.eu>
2019-01-30 16:51:35 +0100

    Add firstName, lastName, email to User shema + config API facebook

--- server/models/user.js
+++ server/models/user.js
@@ -15,11 +15,21 @@ var UserSchema = new Schema({
         type: String,
         required: true
     },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
     facebook: {
-        id: String,
         token: String,
-        name: String,
-        email: String
+        id: String
     }
 });
 
